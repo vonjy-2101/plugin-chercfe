c6956be974d8fffc42962a92223b56e031b38aae
vonjy <ralvonjy@gmail.com>
2017-09-05 11:47:27 +0300

    template file

---
+++ templates/bookin-template.php
@@ -0,0 +1,18 @@
+<div class="wrap">
+<h2><?php _e('Bookin cherche','plugin-chercfe'); ?></h2>
+  <table class="wp-list-table widefat fixed striped pages">
+      <thead>
+        <tr>
+          <th scope="col" style="text-align:left;"><?php _e('Déstinnation', 'plugin-chercfe'); ?></th>
+          <th scope="col" style="text-align:left;"><?php _e('Date', 'plugin-chercfe'); ?> </th>
+          <th scope="col" style="text-align:left;"><?php _e("Port", "plugin-chercfe"); ?></th>
+          <th scope="col" style="text-align:left;"><?php _e("Compagnie", "plugin-chercfe"); ?></th>
+          <th scope="col" style="text-align:left;"><?php _e("Image", "plugin-chercfe"); ?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+        </tr>
+      </tbody>
+    </table>
+</div>
