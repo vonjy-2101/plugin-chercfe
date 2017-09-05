530d6984ab298f3a1bdd2e947a22688e2a478ccf
vonjy <ralvonjy@gmail.com>
2017-09-05 11:46:19 +0300

    rectif structur table

--- plugin-chercfe.php
+++ plugin-chercfe.php
@@ -26,6 +26,7 @@
 	  $bookin_collate = $wpdb->get_charset_collate();
 	  $bookin_structure = "CREATE TABLE IF NOT EXISTS $bookin_cherche (
 	                            `id` int(11) NOT NULL AUTO_INCREMENT,
+	                            `destinnation` varchar(255) CHARACTER SET utf8 NOT NULL,
 	                            `date` varchar(255) CHARACTER SET utf8 NOT NULL,
 	                            `port` varchar(255) CHARACTER SET utf8 NOT NULL,
 	                            `compagnie` varchar(255) CHARACTER SET utf8 NOT NULL,
