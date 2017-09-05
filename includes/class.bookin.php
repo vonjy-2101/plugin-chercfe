b01bfdd7fb80881b9b6e7cf87acfa2057fa24098
vonjy <ralvonjy@gmail.com>
2017-09-05 14:00:36 +0300

    script js

--- includes/class.bookin.php
+++ includes/class.bookin.php
@@ -12,6 +12,7 @@
 	    {
 	        add_action('init', array($this,'create_deliver'));
 	        add_action('admin_menu', array($this,'bookin_menu'));
+	        add_action('wp_enqueue_scripts', 'scripts');
 	    }
 
 	    public function bookin_menu()
@@ -21,7 +22,14 @@
 
 	    public function bookin_template()
 	    {
+	    	$this->scripts();
+	    	echo '<script src="'.plugins_url() .'/plugin-chercfe/assets/js/jquery.min.js'.'"></script>';
 	    	include (BASEPAHT ."/templates/bookin-template.php");
+	    }
+
+	    public function scripts()
+	    {
+	    	wp_enqueue_style('bootstrap', plugins_url().'/plugin-chercfe/assets/bootstrap/css/bootstrap.css',false,true); 
 	    }
 	}
 
